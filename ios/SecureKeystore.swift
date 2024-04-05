13dc99a4454bc90eca22db8c14bf9f901cb70cbf
Ayushi Dey <ayushirakshitdey@gmail.com>
2024-04-05 13:12:09 +0530

    chore(#893, #896): iOS code cleanup and code formatted
    Signed-off-by: Ayushi Dey <ayushirakshitdey@gmail.com>

--- ios/SecureKeystore.swift
+++
@@ -1,8 +0,0 @@
-@objc(SecureKeystore)
-class SecureKeystore: NSObject {
-
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
-}
