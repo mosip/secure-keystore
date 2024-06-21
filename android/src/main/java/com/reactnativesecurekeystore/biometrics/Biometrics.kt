7c280813b89c0e6c02e1b4b7bfabf04b5b929230
Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
2024-06-21 11:56:09 +0530

    [INJIMOB-1517]  add native kotlin artifact
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>

--- android/src/main/java/com/reactnativesecurekeystore/biometrics/Biometrics.kt
+++ android/src/main/java/com/reactnativesecurekeystore/biometrics/Biometrics.kt
@@ -1,109 +1,125 @@
- package com.reactnativesecurekeystore.biometrics
+package com.reactnativesecurekeystore.biometrics
 
-  import android.content.Context
-  import android.os.Build
-  import android.security.keystore.KeyPermanentlyInvalidatedException
-  import android.security.keystore.UserNotAuthenticatedException
-  import android.util.Log
-  import androidx.biometric.BiometricManager
-  import androidx.biometric.BiometricPrompt
-  import androidx.fragment.app.FragmentActivity
-  import com.reactnativesecurekeystore.exception.ErrorCode
-  import com.reactnativesecurekeystore.exception.KeyInvalidatedException
-  import java.security.SignatureException
-  import java.util.concurrent.Executor
-  import java.util.concurrent.Executors
-  import javax.crypto.IllegalBlockSizeException
-  import kotlin.coroutines.resumeWithException
-  import kotlin.coroutines.suspendCoroutine
+import android.os.Build
+import android.security.keystore.KeyPermanentlyInvalidatedException
+import android.security.keystore.UserNotAuthenticatedException
+import android.util.Log
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.biometric.BiometricPrompt.CryptoObject
+import androidx.biometric.BiometricPrompt.PromptInfo
+import androidx.fragment.app.FragmentActivity
+import com.reactnativesecurekeystore.common.Util
+import com.reactnativesecurekeystore.exception.ErrorCode
+import com.reactnativesecurekeystore.exception.KeyInvalidatedException
+import java.security.SignatureException
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import javax.crypto.IllegalBlockSizeException
+import android.content.Context
+import java.lang.Exception
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
-  class Biometrics(
-    private val context: Context
-  ) {
-    private val logTag = "sec"
+class Biometrics(
+    private val context: Context,
+) {
+    private val logTag = Util.getLogTag(javaClass.simpleName)
 
     companion object {
-      var POPUP_TITLE = "Unlock App"
-      var POPUP_DESCRIPTION = "Please use fingerprint to unlock the app"
+        var POPUP_TITLE = "Unlock App"
+        var POPUP_DESCRIPTION = "Please use fingerprint to unlock the app"
 
-      fun updatePopupDetails(title: String, description: String) {
-        POPUP_TITLE = title
-        POPUP_DESCRIPTION = description
-      }
+        fun updatePopupDetails(title: String, description: String) {
+            POPUP_TITLE = title
+            POPUP_DESCRIPTION = description
+        }
     }
 
     suspend fun authenticateAndPerform(
-      createCryptoObject: () -> BiometricPrompt.CryptoObject,
-      action: (BiometricPrompt.CryptoObject) -> Unit,
-      onFailure: (code: Int, message: String) -> Unit
+        createCryptoObject: () -> CryptoObject,
+        action: (CryptoObject) -> Unit,
+        onFailure: (code: Int, message: String) -> Unit,
     ) {
-      try {
-        Log.d(logTag, "Creating CryptoObject")
-        val preAuthCryptoObject = createCryptoObject()
-        Log.d(logTag, "CryptoObject created, calling action")
-        action(preAuthCryptoObject)
-      } catch (e: UserNotAuthenticatedException) {
-        Log.e(logTag, "User not authenticated exception", e)
-        authenticate(createCryptoObject, action, false)
-      } catch (e: IllegalBlockSizeException) {
-        Log.e(logTag, "Illegal block size exception", e)
-        authenticate(createCryptoObject, action, true)
-      } catch (e: SignatureException) {
-        Log.e(logTag, "Signature exception", e)
-        authenticate(createCryptoObject, action, true)
-      } catch (e: KeyPermanentlyInvalidatedException) {
-        Log.e(logTag, "Key permanently invalidated exception", e)
-        throw KeyInvalidatedException()
-      } catch (e: Exception) {
-        Log.e(logTag, "Other exception", e)
-        onFailure(ErrorCode.INTERNAL_ERROR.ordinal, e.message.toString())
-      }
+        try {
+            val preAuthCryptoObject = createCryptoObject()
+            action(preAuthCryptoObject)
+        } catch (e: UserNotAuthenticatedException) {
+            // If key has timeout based biometric auth requirement, Cipher.Init fails and caught here
+            Log.e(logTag, "Calling action failed due to auth exception with user not auth", e)
+            authenticate(createCryptoObject, action, false)
+        } catch (e: Exception) {
+            // If key has every use biometric auth requirement, Cipher.doFinal fails and caught here
+            when (e) {
+                is IllegalBlockSizeException, is SignatureException -> {
+                    Log.e(logTag, "Calling action failed due to auth exception", e)
+                    authenticate(createCryptoObject, action, true)
+                }
+
+                is KeyPermanentlyInvalidatedException -> {
+                    throw KeyInvalidatedException()
+                }
+
+                else -> throw e
+            }
+        } catch (e: Exception) {
+            Log.e(logTag, "Calling action failed due to other exception", e)
+            onFailure(ErrorCode.INTERNAL_ERROR.ordinal, e.message.toString())
+        }
     }
 
     fun isBiometricEnabled(): Boolean {
-      val biometricManager = BiometricManager.from(context)
-      return biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG) == BiometricManager.BIOMETRIC_SUCCESS
+        val biometricManager = BiometricManager.from(context)
+        return biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG) == BiometricManager.BIOMETRIC_SUCCESS
     }
 
     private suspend fun authenticate(
-      createCryptoObject: () -> BiometricPrompt.CryptoObject,
-      action: (BiometricPrompt.CryptoObject) -> Unit,
-      createCryptoObjectSuccess: Boolean
-    ) = suspendCoroutine<Unit> { continuation ->
-      try {
-        val fragmentActivity = context as? FragmentActivity
-          ?: throw NullPointerException("Context is not a FragmentActivity")
+        createCryptoObject: () -> CryptoObject,
+        action: (CryptoObject) -> Unit,
+        createCryptoObjectSuccess: Boolean,
+    ) {
+        return suspendCoroutine { continuation ->
+            val fragmentActivity = context as? FragmentActivity
+                ?: throw NullPointerException("Context is not a FragmentActivity")
+            fragmentActivity.runOnUiThread {
+                try {
+                    val onAuthSuccess: (CryptoObject?) -> Unit =
+                        { cryptoObject -> action(cryptoObject ?: createCryptoObject()) }
+                    val authCallback: BiometricPrompt.AuthenticationCallback =
+                        BiometricPromptAuthCallback(continuation, onAuthSuccess)
+                    val executor: Executor = Executors.newSingleThreadExecutor()
 
-        val onAuthSuccess: (BiometricPrompt.CryptoObject?) -> Unit =
-          { cryptoObject -> action(cryptoObject ?: createCryptoObject()) }
+                    val promptInfo = createPromptInfo()
+                    val biometricPrompt =
+                        BiometricPrompt(context as FragmentActivity, executor, authCallback)
 
-        val authCallback = BiometricPromptAuthCallback(continuation, onAuthSuccess)
-        val executor: Executor = Executors.newSingleThreadExecutor()
+                    if (createCryptoObjectSuccess) {
+                        biometricPrompt.authenticate(promptInfo, createCryptoObject())
+                    } else {
+                        try {
+                            biometricPrompt.authenticate(promptInfo)
+                        } catch (e: Exception) {
+                            Log.e(logTag, e.toString())
+                        }
 
-        val promptInfo = createPromptInfo()
-        val biometricPrompt = BiometricPrompt(fragmentActivity, executor, authCallback)
+                    }
 
-        if (createCryptoObjectSuccess) {
-          biometricPrompt.authenticate(promptInfo, createCryptoObject())
-        } else {
-          biometricPrompt.authenticate(promptInfo)
+                } catch (e: Exception) {
+                    Log.e(logTag, "exception in creating auth prompt: ", e)
+                    continuation.resumeWithException(RuntimeException("exception in creating auth prompt"))
+                }
+            }
         }
-      } catch (e: Exception) {
-        Log.e(logTag, "Exception in creating auth prompt", e)
-        continuation.resumeWithException(RuntimeException("Exception in creating auth prompt"))
-      }
     }
 
-    private fun createPromptInfo(): BiometricPrompt.PromptInfo {
-      val builder = BiometricPrompt.PromptInfo.Builder()
-        .setTitle(POPUP_TITLE)
-        .setDescription(POPUP_DESCRIPTION)
-        .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG)
+    private fun createPromptInfo(): PromptInfo {
+        val builder = PromptInfo.Builder().setTitle(POPUP_TITLE).setDescription(POPUP_DESCRIPTION)
+            .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG)
 
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        builder.setNegativeButtonText("Cancel")
-      }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            builder.setNegativeButtonText("Cancel")
+        }
 
-      return builder.build()
+        return builder.build()
     }
-  }
+}
