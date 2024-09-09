51856ec6ce04be0767e996be176ad2c68e9905c0
abhip2565 <74866247+abhip2565@users.noreply.github.com>
2024-09-09 18:51:40 +0530

    [INJIMOB-1578] upgrade snapshot version to 0.3.0 (#95)
    * [INJIMOB-1578] upgrade snapshot version to 0.3.0
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    * [INJIMOB-1578] update Js wrapper
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    ---------
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>

--- js/android/src/main/java/com/reactnativesecurekeystore/RNModule/SecureKeystoreModule.java
+++ js/android/src/main/java/com/reactnativesecurekeystore/RNModule/SecureKeystoreModule.java
@@ -3,25 +3,19 @@ package com.reactnativesecurekeystore;
 import com.reactnativesecurekeystore.SecureKeystoreImpl;
 import com.reactnativesecurekeystore.KeyGeneratorImpl;
 import com.reactnativesecurekeystore.CipherBoxImpl;
-import android.app.Activity;
 import com.reactnativesecurekeystore.DeviceCapability;
-import android.content.Intent;
-import androidx.annotation.NonNull;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.reactnativesecurekeystore.biometrics.Biometrics;
 import com.reactnativesecurekeystore.common.Util;
+import com.facebook.react.bridge.Callback;
 import kotlin.Unit;
-import android.content.Context;
-import org.jetbrains.annotations.Nullable;
 import kotlin.jvm.functions.Function1;
 import kotlin.jvm.functions.Function2;
-import androidx.fragment.app.FragmentActivity;
-import com.facebook.react.bridge.ActivityEventListener;
 
-public class SecureKeystoreModule extends ReactContextBaseJavaModule implements ActivityEventListener {
+public class SecureKeystoreModule extends ReactContextBaseJavaModule {
     private final KeyGeneratorImpl keyGenerator = new KeyGeneratorImpl();
     private final CipherBoxImpl cipherBox = new CipherBoxImpl();
     private final Biometrics biometrics;
@@ -29,31 +23,18 @@ public class SecureKeystoreModule extends ReactContextBaseJavaModule implements 
     private final DeviceCapability deviceCapability;
     private final String logTag;
 
-    private ReactApplicationContext context;
-
     public SecureKeystoreModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        context = reactContext;
-        Context context = getReactApplicationContext();
-        this.biometrics = new Biometrics();
+        this.biometrics = new Biometrics(reactContext);
         this.keystore = new SecureKeystoreImpl(keyGenerator, cipherBox, biometrics);
         this.deviceCapability = new DeviceCapability(keystore, keyGenerator, biometrics);
         this.logTag = Util.Companion.getLogTag(getClass().getSimpleName());
     }
 
+
     @Override
     public String getName() {
         return "SecureKeystoreModule";
-    }
-
-    @Override
-    public void onNewIntent(Intent intent) {
-        // Handle new intent here if needed
-    }
-
-    @Override
-    public void onActivityResult(Activity activity, int requestCode, int resultCode, @Nullable Intent data) {
-        // Handle activity result here if needed
     }
 
     @ReactMethod(isBlockingSynchronousMethod = true)
@@ -82,11 +63,6 @@ public class SecureKeystoreModule extends ReactContextBaseJavaModule implements 
     }
 
     @ReactMethod(isBlockingSynchronousMethod = true)
-    public String generateKeyPairEC(String alias, boolean isAuthRequired, Integer authTimeout) {
-        return keystore.generateKeyPairEC(alias, isAuthRequired, authTimeout);
-    }
-
-    @ReactMethod(isBlockingSynchronousMethod = true)
     public void generateHmacshaKey(String alias) {
         keystore.generateHmacSha256Key(alias);
     }
@@ -104,7 +80,7 @@ public class SecureKeystoreModule extends ReactContextBaseJavaModule implements 
         Function2<Integer, String, Unit> failureLambda = new Function2<Integer, String, Unit>() {
             @Override
             public Unit invoke(Integer code, String message) {
-                promise.reject(code.toString(), message);
+                promise.reject(code.toString(),message);
                 return Unit.INSTANCE;
             }
         };
@@ -112,13 +88,12 @@ public class SecureKeystoreModule extends ReactContextBaseJavaModule implements 
                 alias,
                 data,
                 successLambda,
-                failureLambda,
-                context.getCurrentActivity()
+                failureLambda
         );
     }
 
     @ReactMethod
-    public void decryptData(String alias, String encryptedText, Promise promise) {
+    public void decryptData(String alias, String encryptedText,Promise promise) {
         Function1<String, Unit> successLambda = new Function1<String, Unit>() {
             @Override
             public Unit invoke(String data) {
@@ -130,16 +105,18 @@ public class SecureKeystoreModule extends ReactContextBaseJavaModule implements 
         Function2<Integer, String, Unit> failureLambda = new Function2<Integer, String, Unit>() {
             @Override
             public Unit invoke(Integer code, String message) {
-                promise.reject(code.toString(), message);
+                promise.reject(code.toString(),message);
                 return Unit.INSTANCE;
             }
         };
 
-        keystore.decryptData(alias, encryptedText, successLambda, failureLambda, context.getCurrentActivity());
+        keystore.decryptData(alias, encryptedText, successLambda, failureLambda);
     }
+
 
     @ReactMethod
     public void generateHmacSha(String alias, String data, Promise promise) {
+
         Function1<String, Unit> successLambda = new Function1<String, Unit>() {
             @Override
             public Unit invoke(String sha) {
@@ -151,7 +128,7 @@ public class SecureKeystoreModule extends ReactContextBaseJavaModule implements 
         Function2<Integer, String, Unit> failureLambda = new Function2<Integer, String, Unit>() {
             @Override
             public Unit invoke(Integer code, String message) {
-                promise.reject(code.toString(), message);
+                promise.reject(code.toString(),message);
                 return Unit.INSTANCE;
             }
         };
@@ -165,7 +142,8 @@ public class SecureKeystoreModule extends ReactContextBaseJavaModule implements 
     }
 
     @ReactMethod
-    public void sign(String signAlgorithm, String alias, String data, Promise promise) {
+    public void sign(String alias, String data, Promise promise) {
+
         Function1<String, Unit> successLambda = new Function1<String, Unit>() {
             @Override
             public Unit invoke(String signature) {
@@ -177,18 +155,17 @@ public class SecureKeystoreModule extends ReactContextBaseJavaModule implements 
         Function2<Integer, String, Unit> failureLambda = new Function2<Integer, String, Unit>() {
             @Override
             public Unit invoke(Integer code, String message) {
-                promise.reject(code.toString(), message);
+                promise.reject(code.toString(),message);
                 return Unit.INSTANCE;
             }
         };
 
         keystore.sign(
-                signAlgorithm,
                 alias,
                 data,
                 successLambda,
-                failureLambda,
-                context.getCurrentActivity()
+                failureLambda
+
         );
     }
 
@@ -199,6 +176,6 @@ public class SecureKeystoreModule extends ReactContextBaseJavaModule implements 
 
     @ReactMethod(isBlockingSynchronousMethod = true)
     public boolean hasBiometricsEnabled() {
-        return deviceCapability.hasBiometricsEnabled(context.getCurrentActivity());
+        return deviceCapability.hasBiometricsEnabled();
     }
 }
